eb428b0da9caad05313b682a577e87c4f68507be
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-27 03:28:50 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,1 +1,14 @@
-# Code your solution here!
+
+def run_guessing_game
+  int = rand(1..6)
+  puts "Guess a number between 1 and 6"
+  input = gets.chomp.strip
+  if input == "exit"
+    puts "Goodbye!"
+  elsif input == int
+    puts "You guessed the correct number!"
+  else
+    puts "Sorry! The computer guessed #{int}"
+  end
+end
+    
