39a8328be0b2a7f4106cb7112ea410060049ed20
bionic-chipset-2986 <janaeelisa@gmail.com>
2019-09-27 03:32:40 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,10 +1,10 @@
 
 def run_guessing_game
   int = rand(1..6)
-  input = gets.strip.to_i
+  input = gets.chomp
   if input == "exit"
     puts "Goodbye!"
-  elsif input == int
+  elsif input.to_i == int
     puts "You guessed the correct number!"
   else
     puts "Sorry! The computer guessed #{int}"
